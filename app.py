e2a183625f7ad45c604edb575ebd3d701e467943
sabinaku <sku@qa.com>
2021-01-29 21:18:22 +0000

    This commit add the following functions, SignIn, update and delete. There are some areas require improvement. But the function are OK.

--- app.py
+++ app.py
@@ -1,8 +1,7 @@
-from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-from flask import Flask, render_template, request, flash
+from flask import Flask, render_template, request, flash, session, url_for, redirect
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField, PasswordField, validators
+from wtforms import StringField, SubmitField, PasswordField, SelectField, validators
 # from wtforms import DataRequired, ValidationError,Email, Length
 
 app = Flask(__name__)
@@ -37,21 +36,146 @@ class LoginForm(FlaskForm):
     password = PasswordField('password', validators=[validators.Length(min=6,max=25)])
     submit = SubmitField('Sign In')
 
+class SignUpForm(FlaskForm):
+    firstname = StringField('First Name',validators=[validators.DataRequired()])
+    lastname = StringField('Last Name',validators=[validators.DataRequired()])
+    email = StringField('email',validators=[validators.Email(granular_message=True)])
+    password = PasswordField('Password', validators=[validators.EqualTo('reenterpassword',message = "Password doesn't match")])
+    reenterpassword = PasswordField('Re-Type Password', validators=[validators.Length(min=6,max=25)])
+    telephone = StringField('Telephone',validators=[validators.DataRequired()])
+    gender = SelectField('Gender',choices=['M','F'])
+    age = StringField('Age',validators=[validators.DataRequired()])
+    submit = SubmitField('Sign Up')
+
+class UpdateForm(FlaskForm):
+    firstname = StringField('First Name',validators=[validators.DataRequired()])
+    lastname = StringField('Last Name',validators=[validators.DataRequired()])
+    email = StringField('email',validators=[validators.Email(granular_message=True)])
+    telephone = StringField('Telephone',validators=[validators.DataRequired()])
+    gender = SelectField('Gender',choices=['M','F'])
+    age = StringField('Age',validators=[validators.DataRequired()])
+    update = SubmitField('Update')
+    delete = SubmitField('Delete')
+
+####Functions
+def is_exist(customers, email):
+    for customer in customers:
+        if (email == customer.email):
+            return True
+    return False
+
+def is_same_password(customers, password, email):
+    for customer in customers:
+        if (email == customer.email) and (password == customer.password):
+            return True
+    return False
+
+def add_customer(frm):
+    first_name = frm.firstname.data
+    last_name = frm.lastname.data
+    email = frm.email.data
+    password = frm.password.data
+    telephone = frm.telephone.data
+    gender = frm.gender.data
+    age = frm.age.data
+    customer = Customers(first_name=first_name, last_name=last_name, email=email,\
+        password = password, telephone=telephone, gender=gender, age=age)
+    return customer
+
 
 @app.route('/', methods=['GET', 'POST'])
 @app.route('/home', methods=['GET', 'POST'])
 def register():
     error = ""
     form = LoginForm()
-
-    if form.validate_on_submit():
-        email = form.email.data
-        return render_template('login.html', form = form, email=email)
+    email = form.email.data
+    password = form.password.data
+    if  not form.validate_on_submit():        
+        if form.email.errors:
+            email=""
+        if form.password.errors:
+            password=""
+        form.email.data = email
+        form.password.data = password
+        return render_template('login.html', form = form)
     else:
-        return render_template('login.html', form = form, email="")
-        
+        if is_exist(Customers.query.all(), form.email.data):
+            if is_same_password(Customers.query.all(), form.password.data, form.email.data):
+                # return render_template('login.html', form = form,message = "existing customer with correct password")
+                session['count'] = 1
+                session['email'] = email
+                return redirect(url_for('update'))
+            else:
+                return render_template('login.html', form = form,message = "Password is incorrect. Please try again.")
 
-    return render_template('login.html', form=form, message = form.email.process_errors)
+        else: # new user
+            session['email'] =  email
+            session['password'] = password
+            session['count'] = 1
+            return redirect(url_for('signup'))
+    #return render_template('signup.html', form = form)
+@app.route('/signup', methods=['GET', 'POST'])
+def signup():
+    form = SignUpForm()
+    #currentpassword = form.password.data
+    if session['count'] ==1:
+        form.email.data = session.get('email')
+        session['count'] = 2
+    
+    if  form.validate_on_submit(): ## everything is OK
+        # if len(Customers.query.all()) ==0:
+        #     return render_template('signup.html', form = form,message = 'New customer')
+        # else:
+        
+        if is_exist(Customers.query.all(), form.email.data):
+            return render_template('signup.html', form = form,message = 'Existing customer')
+        else:
+            customer = add_customer(form)
+            #customer.password = currentpaessword
+            db.session.add(customer)
+            name = customer.first_name +" "+ customer.last_name
+            db.session.commit()
+            output_message = f"Hi {name}, your account has been added. Here is your details."
+            
+            return render_template('update.html', form = form,message = output_message)
+    else:
+        return render_template('signup.html', form = form , message = 'Please check your data')
+    #return render_template('signup.html', form = form)
+
+
+@app.route('/update', methods=['GET', 'POST'])
+def update():
+    form = UpdateForm()
+    currentemail = session['email']
+    customer = Customers.query.filter_by(email=session['email']).first()
+    if session['count'] == 1:
+        form.firstname.data = customer.first_name
+        form.lastname.data = customer.last_name
+        form.email.data = customer.email
+        form.telephone.data = customer.telephone
+        form.gender.data = customer.gender
+        form.age.data = customer.age
+        session['name']= customer.first_name +" "+customer.last_name
+        session['count'] = 2
+    if form.update.data and form.validate_on_submit(): ##update record
+        customer.first_name =form.firstname.data
+        form.lastname.data = customer.last_name
+        customer.email = form.email.data
+        customer.telephone = form.telephone.data
+        customer.gender = form.gender.data
+        customer.age = form.age.data
+        db.session.commit()
+        return render_template('update.html', form = form, message = 'Update record successfully.')
+    if form.delete.data:
+        db.session.delete(customer)
+        db.session.commit()
+        return f"Hi {session['name']}, your account has been deleted.<br/>"
+
+
+
+    return render_template('update.html', form = form)
+
+
 
 if __name__ == '__main__':
      app.run(debug=True, host='0.0.0.0')
--- tables.py
+++ tables.py
@@ -7,10 +7,10 @@ from app import db, Users, Customers
 
 db.create_all()
 
-testuser = Users(first_name='Grooty',last_name='Toot') # Extra: this section populates the table with an example entry
-db.session.add(testuser)
-db.session.commit()
+# testuser = Users(first_name='Grooty',last_name='Toot') # Extra: this section populates the table with an example entry
+# db.session.add(testuser)
+# db.session.commit()
 
-customer = Customers(first_name ='John',last_name ='Dow',email = 'john.dow@company.com',telephone ='1234567',gender = 'M',age =20)
-db.session.add(customer)
-db.session.commit()
+# customer = Customers(first_name ='John',last_name ='Dow',email = 'john.dow@company.com',telephone ='1234567',gender = 'M',age =20)
+# db.session.add(customer)
+# db.session.commit()
